910457149affc771c39ad1b79fd5d87e2cb14ba2
asfaqmoideen <asfaqmoideen@gmail.com>
2024-05-26 14:14:06 +0530

    func: displayDataInSerialMonitor

--- mian_PWA/mian_PWA.ino
+++ mian_PWA/mian_PWA.ino
@@ -2,36 +2,33 @@
  #define DHT_PIN 2
  #define SOILMOISTURE_PIN A0
  DHT dht(DHT_PIN,DHT11);
-   
+
  void setup()  
  {  
   delay(1000);
   Serial.begin(115200);  
   Serial.println();
+  Serial.print("Plan Watering Automation");
+  Serial.print("Initializing..;");
   pinMode(SOILMOISTURE_PIN, INPUT);
   pinMode(DHT_PIN, INPUT);
  }  
  void loop()  
  {  
-   displayDHTDataInSerialMoinotor();
-   displaySoilDataInSerialMonitor();  
+   int soilValue = analogRead(SOILMOISTURE_PIN);
+   displayDataInSerialMonitor(soilValue);  
    delay(1000);  
  }
 
- void displayDHTDataInSerialMoinotor()
- {
-   Serial.print("Temperature in C:");  
-   Serial.println((dht.readTemperature( )));  
-   Serial.print("Humidity in C:");  
-   Serial.println((dht.readHumidity()));  
- }
-
-void displaySoilDataInSerialMonitor()
+void displayDataInSerialMonitor(int soilValue)
 {
-  int soilValue = analogRead(SOILMOISTURE_PIN);
-  double soilMoisturePercenatge = 100 - ((soilValue/1024)*100) ;
+  Serial.print("Temperature in C:");  
+  Serial.println((dht.readTemperature( )));  
+  Serial.print("Humidity in C:");  
+  Serial.println((dht.readHumidity()));
+
   Serial.print("Soil Moisture Percenatge");
-  Serial.println(soilMoisturePercenatge);
+  Serial.println();
   Serial.print("Soil Moisture in Analog Value");
   Serial.println(soilValue);
 }
